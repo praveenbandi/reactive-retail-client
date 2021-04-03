ab91339ddc27ca4a2d32435cfc8399f26f3de037
praveenbandi <ipraveenbandi@gmail.com>
2021-04-03 18:04:15 -0400

    first commit

---
+++ src/main/java/com/vedam/retailclient/RetailclientApplication.java
@@ -0,0 +1,13 @@
+package com.vedam.retailclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vedam/retailclient/RetailclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedam.retailclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
